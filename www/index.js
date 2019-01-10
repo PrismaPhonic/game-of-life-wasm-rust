d07726d63cdb5246e5c9f2e472c7d80d01185cca
Peter Farr <Peter@PrismaPhonic.com>
2019-01-10 00:37:02 -0800

    got www subdirectory back in the project - essential due to large changes in www folder

---
+++ www/index.js
@@ -0,0 +1,163 @@
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+// Construct the universe and get its width and height
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines
+  for (let i = 0; i < width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1)+ 1);
+  }
+
+  ctx.stroke();
+}
+
+const getIndex = (row, column) => {
+  return row * width + column;
+}
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ?
+        DEAD_COLOR :
+        ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+let pulsar = false;
+let ship = false;
+
+const pulsarButton = document.getElementById("pulsar");
+
+pulsarButton.addEventListener("click", event => {
+  pulsar = !pulsar;
+});
+
+canvas.addEventListener("click", event => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  pulsar = event.ctrlKey;
+  ship = event.shiftKey;
+
+  if (pulsar) {
+    universe.add_pulsar(row, col);
+  } else if (ship) {
+    universe.add_spaceship(row, col);
+  } else {
+    universe.toggle_cell(row, col);
+  }
+
+  drawGrid();
+  drawCells();
+});
+
+let animationId = null;
+let fps = 30;
+
+const renderLoop = (timestamp) => {
+  // throttle requestAnimationFrame to 20fps
+  setTimeout(function() {
+
+    drawGrid();
+    drawCells();
+    
+    universe.tick();
+
+    animationId = requestAnimationFrame(renderLoop);
+  }, 1000/fps)
+};
+
+// PLAY PAUSE LOGIC
+
+const isPaused = () => {
+  return animationId === null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  fps = 0;
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+// FPS SLIDER
+const fpsSlider = document.getElementById("fps-slider");
+fpsSlider.addEventListener("input", evt => {
+  fps = evt.target.value;
+});
+
+// CLEAR BOARD
+const clearBoard = document.getElementById("clear-board");
+clearBoard.addEventListener("click", evt => {
+  universe.kill_all();
+  drawGrid();
+  drawCells();
+});
+
+play();
